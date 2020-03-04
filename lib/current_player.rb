5b3cbf1eba6c365c6131af6c3eb48a5edbf47eed
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-04 01:01:21 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  if counter % 2 == 0
+    counter += 1
+    return 'X'
+  else
+    counter += 1
+    return 'O'
+  end
+end
+
+def current_player(board)
+
+end
